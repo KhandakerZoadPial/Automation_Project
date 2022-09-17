593e00264615d36634c0325d1858f852bf410102
KhandakerZoadPial <khpial27@gmail.com>
2022-09-17 16:54:55 +0600

    more changes

--- burt/views.py
+++ burt/views.py
@@ -21,6 +21,10 @@ def burt_(request):
         if 'submit' in request.POST:
             print('submit button pressed')
             import os
+            my_str = """
+            /usr/software/rats/bin/burt report -q'$date_create gt "20220101" && $state =~ /STUDY/ && $type =~ /TESTCASE/ && $subteam =~ /^(0|4|217)$/ && $subtype =~ /platform_scripts/ && ($title =~ /Dagger2|Dagger-2/i || $hw_platform =~ /dagger2/i)  && $keywords =~ /Dg2Au($|\s+|\,)/i' -f':date_create id state sev pri impact type subtype sub_by time_est keywords:30 owner mgr_owner title' -s'id date_create pri sev'
+            """
+            os.system(my_str)
             return render(request, 'burt/result.html')
         elif 'save' in request.POST:
             # save the query in Database
